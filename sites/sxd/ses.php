db41ea88dfdfc842f72fbefbf2b09c2b84a66e5a
Slava Pasechnik <slavapas13@gmail.com>
2017-07-17 17:27:41 +0000

    Init Drupal

---
+++ sites/sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  '3fe5ef54fac9769a691b9ad19c0ee316' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'en',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'slavapa',
+      'my_pass' => '',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1500312120,
+    'lng' => 'en',
+  ),
+);
+?>
